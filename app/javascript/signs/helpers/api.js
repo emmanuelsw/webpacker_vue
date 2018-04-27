f85b521cd6b46228e59907f52d3483e662c1665b
EmmanuelSW <edhgt89@hotmail.com>
2018-04-27 13:36:55 -0500

    Test indent

--- app/javascript/signs/helpers/api.js
+++ app/javascript/signs/helpers/api.js
@@ -4,23 +4,23 @@ let token = document.getElementsByName('csrf-token')[0].getAttribute('content')
 axios.defaults.headers.common['X-CSRF-Token'] = token
 
 export function get(url) {
-	return axios({
-		method: 'GET',
-		url: url
-	})
+  return axios({
+    method: 'GET',
+    url: url
+  })
 }
 
 export function post(url, payload) {
-	return axios({
-		method: 'POST',
-		url: url,
-		data: payload
-	})
+  return axios({
+    method: 'POST',
+    url: url,
+    data: payload
+  })
 }
 
 export function del(url) {
-	return axios({
-			method: 'DELETE',
-			url: url
-	})
+  return axios({
+      method: 'DELETE',
+      url: url
+  })
 }
