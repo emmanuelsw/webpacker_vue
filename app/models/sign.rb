7d470f0fe990310b2d03c46bd65f0a6872495ee1
EmmanuelSW <edhgt89@hotmail.com>
2018-04-27 13:39:01 -0500

    Add .editorconfig

--- app/models/sign.rb
+++ app/models/sign.rb
@@ -1,22 +1,22 @@
 class Sign < ApplicationRecord
-	has_many :goals
+  has_many :goals
 
-	validates :name, presence: true
-	validates :email, presence: true, uniqueness: true, email: true
-	validates :phone, presence: true, numericality: { only_integer: true }
-	validates :birthdate, presence: true
-	validates :sign, presence: true, length: { minimum: 3, too_short: "can't be blank" }
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true, email: true
+  validates :phone, presence: true, numericality: { only_integer: true }
+  validates :birthdate, presence: true
+  validates :sign, presence: true, length: { minimum: 3, too_short: "can't be blank" }
 
-	def birthdate
-		super.strftime("%d %B %Y") unless super.nil?
-	end
+  def birthdate
+    super.strftime("%d %B %Y") unless super.nil?
+  end
 
-	def created_at
-		super.strftime("%d %B %Y %H:%M:%S")
-	end
+  def created_at
+    super.strftime("%d %B %Y %H:%M:%S")
+  end
 
-	def updated_at
-		super.strftime("%d %B %Y %H:%M:%S")
-	end
+  def updated_at
+    super.strftime("%d %B %Y %H:%M:%S")
+  end
 
 end
